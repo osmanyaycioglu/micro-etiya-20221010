37f2989246d7dc1c8ef19fc7c219247d3b1623bc
osman <osman@osy.com>
2022-10-10 13:53:17 +0300

    day 1 2

---
+++ node-discovery/src/main/java/org/training/microservice/nodediscovery/NodeDiscoveryApplication.java
@@ -0,0 +1,16 @@
+package org.training.microservice.nodediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NodeDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeDiscoveryApplication.class,
+                              args);
+    }
+
+}
---
+++ node-discovery/src/test/java/org/training/microservice/nodediscovery/NodeDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.microservice.nodediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
